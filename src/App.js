e979008b77fd2189a97cca5cf0bcc80676ed15f0
jade <jade@deepnatural.ai>
2021-03-12 01:47:12 +0900

    dynamic component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from 'react';
 
-function Food({fav}) {
+function Food({name, image}) {
     return (
-        <h1>I Like {fav}</h1>
+        <div>
+        <h1>I Like {name}</h1>
+        <img src={image} alt=""/>
+        </div>
     )
 }
+
+
+const foodArray = [
+    {
+        'name': '짜장면',
+        'img': 'http://ojsfile.ohmynews.com/STD_IMG_FILE/2016/1214/IE002069160_STD.jpg'
+    },
+    {
+        'name': '국밥',
+        'img': 'https://bit.ly/3qAFypB'
+    },
+    {
+        'name': '라면',
+        'img': 'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg'
+    }
+]
 
 function App() {
     return (
         <div>
             <h1>Foods</h1>
-            <Food fav="kimchi" />
-            <Food fav="ramen" />
-            <Food fav="bab" />
-            <Food fav="hotba" />
+            {foodArray.map(dish => <Food name={dish.name} image={dish.img} />)}
         </div>
     )
 }
